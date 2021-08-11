dc987c8f02e146ebb512be1ec50b7716aa8c334c
SRC0747 <srcsrk1998@gmail.com>
2021-08-11 12:10:02 +0530

    Welcome to Employee Wage Computation Problem.

---
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package EmployeeWageBuilder;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee Wage Computation Problem.");
+    }
+}
